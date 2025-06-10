c2710daffb782cd3fbf1453ff180b31720019f8d
Mel Chen <mel.chen@sifive.com>
2025-06-10 07:57:49 -0700

    IsSingleScalar

--- llvm/lib/Transforms/Vectorize/VPlanUtils.cpp
+++ llvm/lib/Transforms/Vectorize/VPlanUtils.cpp
@@ -290,6 +290,10 @@ bool vputils::isSingleScalar(const VPValue *VPV) {
     return preservesUniformity(WidenR->getOpcode()) &&
            all_of(WidenR->operands(), isSingleScalar);
   }
+  if (auto *CastR = dyn_cast<VPWidenCastRecipe>(VPV)) {
+    return PreservesUniformity(CastR->getOpcode()) &&
+           all_of(CastR->operands(), isSingleScalar);
+  }
   if (auto *VPI = dyn_cast<VPInstruction>(VPV))
     return VPI->isSingleScalar() || VPI->isVectorToScalar() ||
            (preservesUniformity(VPI->getOpcode()) &&
